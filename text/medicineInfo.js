ccfa1869874afd40b26ce695c9c075789b5d5612
O-Julia-O <julia.qq.onix@gmail.com>
2022-07-18 15:17:06 +0300

    deleted some russian text about insurance

--- text/medicineInfo.js
+++ text/medicineInfo.js
@@ -13,7 +13,6 @@ const aboutMedicine = `
 
 <b>Що таке ESI?</b>
 Це ваш номер у системі охорони здоров’я Литви. Відповідно до нього система вас розпізнає. Це дозволить вам лікуватися в литовських лікарнях і поліклініках, а лікарі зможуть призначати вам ліки. З цим кодом ви отримуєте безкоштовні ліки в аптеках.
-<b>Бесплатное медицинское страхование</b> - <a href="https://www.ld.lt/dovanojame-draudima-ukrainos-vaikams">https://www.ld.lt</a>
 <b>Пам’ятка про медичне обслуговування</b> - <a href='https://lrv.lt/uploads/main/documents/files/%D0%9F%D0%B0%D0%BC%D1%8F%D1%82%D0%BA%D0%B0%20%D0%BF%D1%80%D0%BE%20%D0%BC%D0%B5%D0%B4%D0%B8%D1%87%D0%BD%D0%B5%20%D0%BE%D0%B1%D1%81%D0%BB%D1%83%D0%B3%D0%BE%D0%B2%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%D0%B2%20%D0%9B%D0%B8%D1%82%D0%B2%D1%96%20(UA).pdf'>скачати пам’ятку - UA</a>
 <b>Памятка о медицинских услугах</b> - <a href='https://lrv.lt/uploads/main/documents/files/%D0%9F%D0%B0%D0%BC%D1%8F%D1%82%D0%BA%D0%B0%20%D0%BE%20%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D1%85%20%D1%83%D1%81%D0%BB%D1%83%D0%B3%D0%B0%D1%85%20%D0%B2%20%D0%9B%D0%B8%D1%82%D0%B2%D0%B5%20(RU).pdf'>Скачать пямятку - RU</a>
 

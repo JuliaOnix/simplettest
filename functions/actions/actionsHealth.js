21c59f0085a7e5e7c27e91606bc913376e36742c
O-Julia-O <julia.qq.onix@gmail.com>
2022-09-18 06:47:33 +0300

    fdfd

--- functions/actions/actionsHealth.js
+++ functions/actions/actionsHealth.js
@@ -1,8 +1,4 @@
-const {Markup} = require("telegraf");
-const constans = require('../fileCostants')
-const freeMedInfo = require('../../text/freeMedServices')
-const medInfo = require('../../text/medicineInfo')
-const MAIN_MENU = require('../updates_20_09_22/Main_Manu')
+
 
 /**
 
@@ -52,72 +48,3 @@ showCovidPage = handleAction('corona')
 
 */
 
-async function showDentalPage(ctx) {
-    console.log(`${ctx.from.username} dental_btn choosed`)
-    await ctx.answerCbQuery();
-    await ctx.editMessageText(freeMedInfo.dentist, {
-        parse_mode: 'HTML',
-        disable_web_page_preview: true, 
-        protect_content: true,
-        ...Markup.inlineKeyboard([
-            [Markup.button.callback(constans.RETURN_BACK_TO_MENU, 'returnBack_btn')]
-        ])
-    });
-}
-
-async function showMentalPage(ctx) {
-    console.log(`${ctx.from.username} mentalHealth_btn choosed`)
-    await ctx.answerCbQuery();
-    await ctx.editMessageText(freeMedInfo.mentalHelth, {
-        parse_mode: 'HTML',
-        disable_web_page_preview: true,
-        protect_content: true,
-        ...Markup.inlineKeyboard([
-            [Markup.button.callback(constans.RETURN_BACK_TO_MENU, 'returnBack_btn')]
-        ])
-    });
-}
-
-async function showDiabetPage(ctx) {
-    console.log(`${ctx.from.username} diabet_btn choosed`)
-    await ctx.answerCbQuery();
-    await ctx.editMessageText(freeMedInfo.diabetPost, {
-        parse_mode: 'HTML',
-        disable_web_page_preview: true,
-        protect_content: true,
-        ...Markup.inlineKeyboard([
-            [Markup.button.callback(constans.RETURN_BACK_TO_MENU, 'returnBack_btn')]
-        ])
-    });
-}
-
-async function showCovidPage(ctx) {
-    console.log(`${ctx.from.username} covid_btn choosed`)
-    await ctx.answerCbQuery();
-    await ctx.editMessageText(freeMedInfo.corona, {
-        parse_mode: 'HTML',
-        disable_web_page_preview: true,
-        protect_content: true,
-        ...Markup.inlineKeyboard([
-            [Markup.button.callback(constans.RETURN_BACK_TO_MENU, 'returnBack_btn')]
-        ])
-    });
-}
-
-async function returnBack(ctx) {
-    console.log(`${ctx.from.username} returnBack_btn choosed`)
-    /* return infoAboutMedicineFunc(ctx); */
-    await ctx.answerCbQuery();
-    return await ctx.editMessageText(medInfo.aboutMedicine, {
-        parse_mode: 'HTML',
-        disable_web_page_preview: true,
-        protect_content: true,
-        ...Markup.inlineKeyboard(MAIN_MENU.MENU_HEALTH)
-    })
-}
-
-module.exports.showDentalPage = showDentalPage;
-module.exports.showCovidPage = showCovidPage;
-module.exports.showMentalPage = showMentalPage;
-module.exports.showDiabetPage = showDiabetPage;
-module.exports.returnBack = returnBack; 
